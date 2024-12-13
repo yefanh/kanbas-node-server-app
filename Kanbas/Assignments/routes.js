df9c93b72987827a0b6aa4b1bd4dcc19e3e3e77f
Yefan He <hyf528@outlook.com>
2024-12-12 21:54:09 -0500

    accidently delete recover

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -4,28 +4,28 @@ import * as assignmentsDao from "./dao.js";
 
 export default function AssignmentRoutes(app) {
   // get specific assignment
-  app.get("/api/courses/:courseId/assignments", (req, res) => {
+  app.get("/api/courses/:courseId/assignments", async (req, res) => {
     const { courseId } = req.params;
-    const assignments = assignmentsDao.findAssignmentsForCourse(courseId);
+    const assignments = await assignmentsDao.findAssignmentsForCourse(courseId);
     res.json(assignments);
   });
 
   // create a new assignment
-  app.post("/api/courses/:courseId/assignments", (req, res) => {
+  app.post("/api/courses/:courseId/assignments", async (req, res) => {
     const { courseId } = req.params;
     const assignment = {
       ...req.body,
       course: courseId,
     };
-    const newAssignment = assignmentsDao.createAssignment(assignment);
+    const newAssignment = await assignmentsDao.createAssignment(assignment);
     res.json(newAssignment);
   });
 
   // uodate a specific assignment
-  app.put("/api/assignments/:assignmentId", (req, res) => {
+  app.put("/api/assignments/:assignmentId", async (req, res) => {
     const { assignmentId } = req.params;
     const assignmentUpdates = req.body;
-    const updatedAssignment = assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
+    const updatedAssignment = await assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
     if (updatedAssignment) {
       res.json(updatedAssignment);
     } else {
@@ -34,9 +34,9 @@ export default function AssignmentRoutes(app) {
   });
 
   // delete a specific assignment
-  app.delete("/api/assignments/:assignmentId", (req, res) => {
+  app.delete("/api/assignments/:assignmentId", async (req, res) => {
     const { assignmentId } = req.params;
-    const success = assignmentsDao.deleteAssignment(assignmentId);
+    const success = await assignmentsDao.deleteAssignment(assignmentId);
     if (success) {
       res.sendStatus(204);
     } else {
--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -3,49 +3,49 @@ import * as enrollmentsDao from "./dao.js";
 
 export default function EnrollmentRoutes(app) {
   // user enroll in course
-  app.post("/api/enrollments/:courseId", (req, res) => {
+  app.post("/api/enrollments/:courseId", async (req, res) => {
     const currentUser = req.session["currentUser"];
     if (!currentUser) {
       res.sendStatus(401);
       return;
     }
     const { courseId } = req.params;
-    const enrollment = enrollmentsDao.enrollUserInCourse(currentUser._id, courseId);
+    const enrollment = await enrollmentsDao.enrollUserInCourse(currentUser._id, courseId);
     res.json(enrollment);
   });
 
   // user unenroll from course
-  app.delete("/api/enrollments/:courseId", (req, res) => {
+  app.delete("/api/enrollments/:courseId", async (req, res) => {
     const currentUser = req.session["currentUser"];
     if (!currentUser) {
       res.sendStatus(401);
       return;
     }
     const { courseId } = req.params;
-    enrollmentsDao.unenrollUserFromCourse(currentUser._id, courseId);
+    await enrollmentsDao.unenrollUserFromCourse(currentUser._id, courseId);
     res.sendStatus(204);
   });
 
   // check if user is enrolled in course
-  app.get("/api/enrollments/:courseId", (req, res) => {
+  app.get("/api/enrollments/:courseId", async (req, res) => {
     const currentUser = req.session["currentUser"];
     if (!currentUser) {
       res.sendStatus(401);
       return;
     }
     const { courseId } = req.params;
-    const isEnrolled = enrollmentsDao.isUserEnrolledInCourse(currentUser._id, courseId);
+    const isEnrolled = await enrollmentsDao.isUserEnrolledInCourse(currentUser._id, courseId);
     res.json({ enrolled: isEnrolled });
   });
 
   // get courses of current user chosen
-  app.get("/api/users/current/courses", (req, res) => {
+  app.get("/api/users/current/courses", async (req, res) => {
     const currentUser = req.session["currentUser"];
     if (!currentUser) {
       res.sendStatus(401);
       return;
     }
-    const courses = enrollmentsDao.findCoursesForUser(currentUser._id);
+    const courses = await enrollmentsDao.findCoursesForUser(currentUser._id);
     res.json(courses);
   });
 }
